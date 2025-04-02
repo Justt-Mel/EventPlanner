9ac1aede9781863c867f0dd7303fbb69d4baca63
Justt-Mel <justinmelendezj@gmail.com>
2025-04-02 12:16:59 -0400

    created the function to pull and get the data from the api

--- event.js
+++ event.js
@@ -1,2 +1,15 @@
+//creating the state to hold the events data 
+const events = [];
+
+//creating link to the div in the html file
+const eventList = document.querySelector("#event");
 //linking api to the Js File 
-const apiUrl = 'https://fsa-crud-2aa9294fe819.herokuapp.com/api/2501-ftb-et-web-am/events';
+const getEvent = async () => 
+{
+    const plan = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2501-ftb-et-web-am/events")
+    console.log(plan)
+    const json = await plan.json()
+    console.log(json)
+}
+//creating a function to display the events in the html file
+getEvent();
