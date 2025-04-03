367987ec56487a98cc547ee7da78cdf2ee99d510
Justt-Mel <justinmelendezj@gmail.com>
2025-04-03 00:44:09 -0400

    started my render function

--- event.js
+++ event.js
@@ -3,13 +3,33 @@ const events = [];
 
 //creating link to the div in the html file
 const eventList = document.querySelector("#event");
+
 //linking api to the Js File 
 const getEvent = async () => 
+    {
+        const plan = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2501-ftb-et-web-am/events")
+        //console.log(plan)
+        const json = await plan.json()
+       // console.log(json.data)
+        events= json.data;
+        render()
+    }
+
+// creatin g a render function 
+const render = () => 
 {
-    const plan = await fetch("https://fsa-crud-2aa9294fe819.herokuapp.com/api/2501-ftb-et-web-am/events")
-    console.log(plan)
-    const json = await plan.json()
-    console.log(json)
+    const added = events.map((planner) =>
+    {
+        return `
+          <div>
+            <h2>${planner.name}</h2>
+            <p>${planner.description}
+            </p>
+          </div>
+        `
+    })
+    eventList.innerHTML = added.join("")
 }
+
 //creating a function to display the events in the html file
 getEvent();
